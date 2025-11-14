46252fba74abce685444d991d6892183ed3b1f77
Zeba <saidzodazebida@gmail.com>
2025-11-14 16:59:28 +0300

    Remove elements from a list

--- app/src/main/java/com/example/collections/MainActivity.kt
+++ app/src/main/java/com/example/collections/MainActivity.kt
@@ -13,13 +13,16 @@ fun main() {
     solarSystem.add(3, "Theia")
     solarSystem[3] = "Future Moon"
 //    println(solarSystem.size)
-    println(solarSystem[3])
-    println(solarSystem[9])
+//    println(solarSystem[3])
+    solarSystem.removeAt(9)
+    solarSystem.remove("Future Moon")
+    println(solarSystem.contains("Pluto"))
+    println("Future Moon" in solarSystem)
 //    println(solarSystem.indexOf("Earth"))
 //    println(solarSystem.indexOf("Pluto"))
-
-    for (planet in solarSystem) {
-        println(planet)
-    }
+//
+//    for (planet in solarSystem) {
+//        println(planet)
+//    }
 
 }
