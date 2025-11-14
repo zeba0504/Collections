7b8d223d4a0ffcfc420eda9da91ac34481c05883
Zeba <saidzodazebida@gmail.com>
2025-11-14 17:20:29 +0300

    Maps

--- app/src/main/java/com/example/collections/MainActivity.kt
+++ app/src/main/java/com/example/collections/MainActivity.kt
@@ -59,7 +59,28 @@ fun exampleSets(){
 }
 
 fun exampleMaps(){
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
 
+    println(solarSystem.size)
+    solarSystem["Pluto"] = 5
+    println(solarSystem.size)
+    println(solarSystem["Pluto"])
+    println(solarSystem.get("Theia"))
+
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
+
+    solarSystem["Jupiter"] = 78
+    println(solarSystem["Jupiter"])
 
 }
 
