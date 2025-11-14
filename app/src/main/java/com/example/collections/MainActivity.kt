296c1ad4213207be59bd84ba1adb7b4018f034ad
Zeba <saidzodazebida@gmail.com>
2025-11-14 17:07:28 +0300

    Arrays

--- app/src/main/java/com/example/collections/MainActivity.kt
+++ app/src/main/java/com/example/collections/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.example.collections
 
-val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
-val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
-val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
-val solarSystem = rockPlanets + gasPlanets
+fun exampleArrays(){
+    val rockPlanets = arrayOf("Mercury", "Venus", "Earth", "Mars")
+    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem = rockPlanets + gasPlanets
+    println(solarSystem[0])
+    println(solarSystem[1])
+    println(solarSystem[2])
+    println(solarSystem[3])
+    println(solarSystem[4])
+    println(solarSystem[5])
+    println(solarSystem[6])
+    println(solarSystem[7])
+
+    solarSystem[3] = "Little Earth"
+    println(solarSystem[3])
+
+    val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
+    println(newSolarSystem[8])
+}
+
+
+
 
 
 
 fun main() {
-    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
-    solarSystem.add("Pluto")
-    solarSystem.add(3, "Theia")
-    solarSystem[3] = "Future Moon"
-//    println(solarSystem.size)
-//    println(solarSystem[3])
-    solarSystem.removeAt(9)
-    solarSystem.remove("Future Moon")
-    println(solarSystem.contains("Pluto"))
-    println("Future Moon" in solarSystem)
-//    println(solarSystem.indexOf("Earth"))
-//    println(solarSystem.indexOf("Pluto"))
-//
-//    for (planet in solarSystem) {
-//        println(planet)
-//    }
+    exampleArrays()
 
 }
